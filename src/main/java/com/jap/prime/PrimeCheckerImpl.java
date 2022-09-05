2a2391d65295fe637f470fcb45f655926f10fa97
Rohit Raj <rohit2000lmp@gmail.com>
2022-09-05 16:25:50 +0530

    final submit

--- src/main/java/com/jap/prime/PrimeCheckerImpl.java
+++ src/main/java/com/jap/prime/PrimeCheckerImpl.java
@@ -1,9 +1,29 @@
 package com.jap.prime;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PrimeCheckerImpl {
     public boolean primeNumberCheck(int number) {
         // write the logic here to check is a number is prime or not
-        return false;
+        PrimeChecker primeChecker = number1 -> {
+            int count =0;
+            boolean flag = false;
+            if (number == 0 || number == 1){
+                return flag;
+            }
+            for (int i = 2; i <number; i++) {
+                if(number%i == 0){
+                    count++;
+                }
+            }
+            if (count == 0){
+                flag = true;
+            }
+            return flag;
+        };
+        return primeChecker.isPrimeNumber(number);
     }
-
 }
+
+
+
